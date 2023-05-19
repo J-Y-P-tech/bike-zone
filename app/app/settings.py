d07bcede9dfe8da7aafd5b6c8feac2ea620d5471
J-Y-P-tech <jordan.yurukov@gmail.com>
2023-05-19 11:58:54 +0300

    Updated settings.py so that it uses SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY') for GitHub Actions only

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-# from .config import *
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+try:
+    from .config import *
+except ImportError:
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

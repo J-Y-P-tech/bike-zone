74596ca93aa220382cec71c28709f1f68ef94721
J-Y-P-tech <jordan.yurukov@gmail.com>
2023-05-19 13:13:26 +0300

    Updated settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-try:
-    from .config import *
-except ImportError:
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+# try:
+#     from .config import *
+# except ImportError:
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

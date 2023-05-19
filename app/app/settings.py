f2b1503232576dec32109bf76d18acd558667f14
J-Y-P-tech <jordan.yurukov@gmail.com>
2023-05-19 13:22:35 +0300

    Fixed GitHUb Actions issue.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# try:
-#     from .config import *
-# except ImportError:
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+try:
+    from .config import *
+except ImportError:
+    SECRET_KEY = 'django-insecure-h4$p#!7*fogx5ann3)k86&wg28sgn0pn(rtn_fia-(*84z+%=$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

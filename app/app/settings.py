def4b827ab89e33c1f5dc33062ead81cd79b4bea
J-Y-P-tech <jordan.yurukov@gmail.com>
2023-05-19 12:10:17 +0300

    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+print(f"os.environ.get('DJANGO_SECRET_KEY') - {os.environ.get('DJANGO_SECRET_KEY')}")
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
     from .config import *
